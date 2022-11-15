4803d8fbd47e6ba93514bedb8d9eed2e757fde1d
Denise Nanni <denise.nanni1@gmail.com>
2022-11-15 16:35:05 +0100

    82 controller implemented

--- 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,39 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
 
+    private static final String DEFAULT_FILE = System.getProperty("user.home") + File.separator + "output.txt";
+
+    private String filename;
+
+    public Controller() {
+        this.filename = DEFAULT_FILE;
+    }
+
+    public String getCurrentFilePath() {
+        return this.filename;
+    }
+
+    public void setCurrentFile(final String filename) {
+        this.filename = filename;
+    }
+
+    public File getCurrentFile() {
+        return new File(this.filename);
+    }
+
+    public void writeOnFile(final String textToWrite) throws IOException {
+        final PrintStream ps = new PrintStream(getCurrentFile());
+
+        ps.println(textToWrite);
+
+        ps.close();
+    }
 }
