4b0e1f5881891bb8c94360590622cd0f1c92f408
denise.nanni <denise.nanni1@gmail.com>
2022-11-21 20:03:44 +0100

    83 model of the controller

--- 83-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 83-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,8 +1,17 @@
 package it.unibo.mvc;
 
+import java.util.List;
+
 /**
- *
+ * Interface that models a controller.
  */
 public interface Controller {
 
+    void setNextString(String next);
+
+    void getNextString();
+
+    List<String> getPrintHistory();
+
+    void printNextString();
 }
