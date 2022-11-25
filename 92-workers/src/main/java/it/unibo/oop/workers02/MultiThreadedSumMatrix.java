be0431e1a0ba7903809e02745fd43a5442410611
Denise Nanni <denise.nanni1@gmail.com>
2022-11-25 13:22:27 +0100

    92 done

--- 92-workers/src/main/java/it/unibo/oop/workers02/MultiThreadedSumMatrix.java
+++ 92-workers/src/main/java/it/unibo/oop/workers02/MultiThreadedSumMatrix.java
@@ -1,79 +1,74 @@
 package it.unibo.oop.workers02;
 
-import java.util.List;
 import java.util.ArrayList;
-import java.util.concurrent.ArrayBlockingQueue;
+import java.util.List;
 
-public class MultiThreadedSumMatrix implements SumMatrix {
+import javax.crypto.spec.IvParameterSpec;
+
+public final class MultiThreadedSumMatrix implements SumMatrix {
 
     private final int nthread;
 
-    /**
-     * 
-     * @param nthread number of threads which will perform the sum
-     */
     public MultiThreadedSumMatrix(final int nthread) {
         this.nthread = nthread;
     }
 
     private static class Worker extends Thread {
-
         private final double[][] matrix;
-        private final int startRow, startCol, nElem;
-        private double res;
+        private final int startRow, startCol;
+        private double result;
+        private final int nelem;
 
-        Worker(final double[][] matrix, final int startRow, final int startCol, final int nElem) {
+        Worker(final double[][] matrix, final int startX, final int startY, final int nelem) {
             this.matrix = matrix;
-            this.startRow = startRow;
-            this.startCol = startCol;
-            this.nElem = nElem;
+            this.startRow = startX;
+            this.startCol = startY;
+            this.nelem = nelem;
         }
 
         @Override
         public void run() {
 
-            System.out.println("Starting from [" + this.startRow + ", " + 
-                this.startCol + "] summing " + this.nElem);
+            System.out.println("Working from [" + this.startRow + ", " + this.startCol + "]"); // NOPMD
 
             int row = this.startRow, col = this.startCol;
-            //System.out.println("AO " + startCol + " BABY " + startRow + " ROBE " + matrix[startRow][startCol]);
 
-            for (int n = 0; n < this.nElem; n++) {
+            for (int n = 0; n < this.nelem; n++) {
                 if (!isWithinBoundary(col, this.matrix[0].length)) {
                     row += 1;
                     col = 0;
                 }
 
                 if (isWithinBoundary(row, this.matrix.length)) {
-                    this.res += this.matrix[row][col];
+                    this.result += this.matrix[row][col];
                 }
 
                 col++;
             }
         }
 
-        private boolean isWithinBoundary(final int index, final int threshold) {
-            if (index >= threshold) {
+        private boolean isWithinBoundary(final int value, final int boundary) {
+            if (value >= boundary) {
                 return false;
             }
 
             return true;
         }
 
-        public double getResult() {
-            return this.res;
+        private double getResult() {
+            return this.result;
         }
     }
 
     @Override
     public double sum(final double[][] matrix) {
         final int rows = matrix.length, columns = matrix[0].length;
-        final int size = rows * columns;
-        final int parts = size % nthread + size / nthread;
-
+        final int size = matrix.length * matrix[0].length;
+        final int nelem = size % this.nthread + size / nthread;
+       
         final List<Worker> workers = new ArrayList<>();
-        for (int i = 0; i < size; i += parts) {
-            workers.add(new Worker(matrix, i != 0 ? i / rows - 1 : i / rows, i % columns, parts));
+        for (int i = 0; i <= size; i += nelem) {
+            workers.add(new Worker(matrix, i / rows, i % columns, nelem));
         }
 
         for (final Worker w : workers) {
@@ -91,5 +86,8 @@ public class MultiThreadedSumMatrix implements SumMatrix {
         }
 
         return result;
+
     }
+
+
 }
